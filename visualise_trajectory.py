752c63e6d9eb94575c1d3ad428bda6d10f465e2e
florianst <florian.stortz@cs.ox.ac.uk>
2019-03-02 15:31:21 +0000

    implemented distance visualisation in feature space

--- visualise_trajectory.py
+++ visualise_trajectory.py
@@ -10,15 +10,19 @@ import numpy as np
 import matplotlib
 matplotlib.use('agg') # different backend so we don't need tkinter
 import matplotlib.pyplot as plt
+import networkx as nx
+#import pygraphviz as pg
+import string
 
 
-def dblWell(x): # double well
+
+def dblWell(x, bias=0): # double well
     E0=1
     E1=0.05
     center_x = 5
     
     x = x-center_x
-    return E0*(E1*x**4-x**2)
+    return E0*(E1*x**4-x**2+bias*x**3)
 
 def fractalLike(x, L=10): # fractal-like Fourier series from Ann. Stat. 34, 1636
     coeffs = [.21, 1.25, .61, .25, .13, .10, 1.16, .18, .12, .23, .21, .19, .37, .99, .36, .02, .06, .08, .09, .04]
@@ -55,6 +59,24 @@ class threeLayerNet(nn.Module): # from https://github.com/MorvanZhou/PyTorch-Tut
         x = self.predict(x)             # linear output
         return x    
 
+def getCurrentWeights(net, indices): # extract weights of layers in indices in form of a python list from a pytorch net
+    weightsReturn = []
+    for index in indices:
+        layerWeights = list(net.parameters())[index][0] # extract weights from parameter set
+        layerWeights = layerWeights.data.numpy()        # convert from pytorch tensor to numpy array
+        #reluWeights = layerWeights.tolist()            # convert from numpy array to python list
+        weightsReturn.append(np.copy(layerWeights))     # return a copy, since usually net.parameters() gives an iterator reference    
+    return weightsReturn
+
+def getDistanceMatrix(weights, magnify=1):
+    distance = np.zeros((np.shape(weights)[0], np.shape(weights)[0]))
+    for t1 in range(np.shape(weights)[0]):
+        for t2 in range(t1):
+            # sum over element-wise squared differences
+            for layer in range(np.shape(weights)[1]):
+                distance[t1, t2] += sum(np.square(weights[t1][layer]-weights[t2][layer]))*magnify
+                distance[t2, t1] = distance[t1, t2] # this distance metric is symmetric
+    return distance
 
 if __name__ == "__main__":
     # sample from function
@@ -64,16 +86,15 @@ if __name__ == "__main__":
     L = 10 # sample points between 0 and this value
     X = L*np.random.rand(N)
     
-    samplingFunction = fractalLike
+    def samplingFunction(X):
+        return dblWell(X, bias=-5e-2)
+        #return fractalLike(X, L)
     y = samplingFunction(X) # choose which function to sample from
     
-    X_train = X[:N_train] # training data
-    y_train = y[:N_train]
-    X_test = X[N_train:]  # validation data
-    y_test = y[N_train:]
-    
-    X_train = X_train.reshape(-1,1)
-    y_train = y_train.reshape(-1,1)
+    X_train = X[:N_train].reshape(-1,1) # training data
+    y_train = y[:N_train].reshape(-1,1)
+    X_test = X[N_train:].reshape(-1,1)  # validation data
+    y_test = y[N_train:].reshape(-1,1)
 
     
     mode = "torch"
@@ -103,6 +124,7 @@ if __name__ == "__main__":
         
         # characterise neural net
         H1,H2,H3,H4,H5 = 80,60,40,40,20
+        layerIndices = [0,2,4,6,8] # used later in weight extraction
         
         net = nn.Sequential(nn.Linear(x.shape[1],H1),    # define the network
                             nn.ReLU(), 
@@ -121,6 +143,8 @@ if __name__ == "__main__":
         optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
         loss_func = nn.MSELoss()  # mean squared loss for regression
         
+        weights = []
+        
         for t in range(epochs):
             prediction = net(x)     # input x and predict yhat based on x
             loss = loss_func(prediction, y)     # must be (1. nn output, 2. target)
@@ -131,15 +155,39 @@ if __name__ == "__main__":
             
             if t % int(epochs/20) == 0:
                 # show learning process
-                print('Loss=%.4f' % loss.data.numpy())
+                print('training loss = %.4f' % loss.data.numpy())
                 if (progress_plots):
                     plt.cla()
                     plt.scatter(x.data.numpy(), y.data.numpy())
                     plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
                     plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 15, 'color':  'red'})
                     plt.savefig('tmp'+str(t)+'.jpg')
+                
+                # save all current weights for visualisation    
+                currentWeights = getCurrentWeights(net, layerIndices)
+                weights.append(currentWeights)
+                    
+        # print loss on test set
+        xtest = torch.from_numpy(X_test).float()
+        ytest = torch.from_numpy(y_test).float()
+        testloss = loss_func(net(xtest), ytest)
+        print("-------- test loss = %.4f" % testloss.data.numpy())
+
+        # calculate distance matrix between the sets of weights for every time step
+        distance = getDistanceMatrix(weights, magnify=15)
         
+        # draw distance graph
+        dt = [('len', float)]
+        distance = distance.view(dt)
+        graph = nx.from_numpy_matrix(distance)
+        graph = nx.drawing.nx_agraph.to_agraph(graph)
+        graph.node_attr.update(colorList="blue,red", size="0.01", style="filled")
+        graph.edge_attr.update(style="invis")  # hide edges
+        graph.draw('graph.png', format='png', prog='neato')
+        
+        # draw prediction
         plotrange = np.linspace(0, L, num=120)
+        plt.figure()
         plt.plot(plotrange, samplingFunction(plotrange), label='true fct.')
         plt.plot(X_train, y_train, '.', label='training data')
         plt.plot(X_test, y_test, 'r.', label='test data')
@@ -149,7 +197,7 @@ if __name__ == "__main__":
         
         plt.plot(plotrange, prediction.data.numpy(), 'k.', label='NN prediction')
         plt.legend()
-        plt.title('Loss=%.4f' % loss.data.numpy())
+        plt.title('Test Loss=%.4f' % testloss)
         
         plt.savefig('prediction.jpg')
     
